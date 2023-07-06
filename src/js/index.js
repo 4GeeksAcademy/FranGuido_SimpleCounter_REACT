a41ca1d5be5331e92e16993ef4fffafb6dfc1500
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-07-06 01:58:11 +0000

    added alert message at 5 s

--- src/js/index.js
+++ src/js/index.js
@@ -3,33 +3,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import propTypes from 'prop-types';
 
+
+
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
 
-let Counter = (props) =>{
+// counter component
+
+function Counter (props) {
     return (
-            <div className="container mt-4 rounded">
-                <div className="row">
-                    <div className="col rounded">
-                    <i className="fas fa-stopwatch shake-lr"></i>
-                    </div>
-                    <div className="col rounded">{props.sixthDigit %10}</div>
-                    <div className="col rounded">{props.fifthDigit %10}</div>
-                    <div className="col rounded">{props.fourthDigit %10}</div>
-                    <div className="col rounded">{props.thirdDigit %10}</div>
-                    <div className="col rounded">{props.secondDigit %10}</div>
-                    <div className="col rounded">{props.firstDigit %10}</div>
+        <div className="container mt-4 rounded">
+            <div className="row">
+                <div className="col rounded">
+                    <i className="fas fa-stopwatch"></i>
                 </div>
-            </div>
+                <div className="col rounded">{props.sixthDigit % 10}</div>
+                <div className="col rounded">{props.fifthDigit % 10}</div>
+                <div className="col rounded">{props.fourthDigit % 10}</div>
+                <div className="col rounded">{props.thirdDigit % 10}</div>
+                <div className="col rounded">{props.secondDigit % 10}</div>
+                <div className="col rounded">{props.firstDigit % 10}</div>
+            </div>  
+            <div>{counterMessage}</div>       
+        </div>
     )
 
 }
 
+
 // defining props
-Counter.propTypes ={
+Counter.propTypes = {
     firstDigit: propTypes.number,
     secondDigit: propTypes.number,
     thirdDigit: propTypes.number,
@@ -38,21 +44,32 @@ Counter.propTypes ={
     sixthDigit: propTypes.number,
 }
 
+const myTimeout = setTimeout(counterMessage, 7000);
+
+function counterMessage () {
+  alert("IT'S BEEN 5 SECONDS.")
+}
+
+function myStopFunction() {
+  clearTimeout(myTimeout);
+}
+
 
 // timer setting
 let timer = 0;
 
-setInterval( function (){
-    let sixth = Math.floor(timer/100000);
-    let fifth = Math.floor(timer/10000);
-    let fourth = Math.floor(timer/1000);
-    let third = Math.floor(timer/100);
-    let second = Math.floor(timer/10);
-    let first = Math.floor(timer/1);
-    timer ++,
+setInterval(function () {
+    let sixth = Math.floor(timer / 100000);
+    let fifth = Math.floor(timer / 10000);
+    let fourth = Math.floor(timer / 1000);
+    let third = Math.floor(timer / 100);
+    let second = Math.floor(timer / 10);
+    let first = Math.floor(timer / 1);
+    timer++,
 
-    //render your react application
-    ReactDOM.render(<Counter firstDigit={first} secondDigit={second} thirdDigit={third} fourthDigit={fourth} fifthDigit={fifth} sixthDigit={sixth}/>, document.querySelector("#app"));
+        //render your react application
+        ReactDOM.render(<Counter firstDigit={first} secondDigit={second} thirdDigit={third} fourthDigit={fourth} fifthDigit={fifth} sixthDigit={sixth} />, document.querySelector("#app"));
 }, 1000)
+
 
 
